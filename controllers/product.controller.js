3c70c7d895977fe8dca33495d23d7c78d35f5d13
YT829 <yoelthe2800@gmail.com>
2022-06-20 02:31:28 +0800

    Update product.controller.js
    Finished products CRUD + Index functions

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,10 @@
 const conn = require('../db')
 
-
+/**
+ * Get 1 product from DB
+ * @param {*} req 
+ * @param {*} res 
+ */
 async function read(req, res) {
     conn.query(`SELECT * FROM products WHERE id = ${req.params.product}`,
         function (err, result) {
@@ -9,6 +13,10 @@ async function read(req, res) {
         })
 }
 
+/**
+ * Get all products from DB
+ * @param {} res 
+ */
 async function index(res) {
     conn.query(`SELECT * FROM products`, function (err, result) {
         if (err) throw err;
@@ -16,6 +24,11 @@ async function index(res) {
     });
 }
 
+/**
+ * Post 1 product into DB
+ * @param {*} req 
+ * @param {*} res 
+ */
 async function create(req, res) {
     const product = req.body,
         data = []
@@ -34,12 +47,29 @@ async function create(req, res) {
 }
 
 
-async function update() {
-
+/**
+ * Update 1 product in DB
+ * @param {*} req 
+ * @param {*} res 
+ */
+async function update(req, res) {
+    conn.query(`UPDATE \`goodies\`.\`products\` SET ? WHERE \`id\`=${req.params.product}`, [req.body], function (err, result) {
+        if (err) throw err;
+        result.data = req.body
+        res.status(201).json(result).end()
+    })
 }
 
-async function destroy() {
-
+/**
+ * Delete 1 product from DB
+ * @param {*} req 
+ * @param {*} res 
+ */
+async function destroy(req, res) {
+    conn.query(`DELETE FROM \`goodies\`.\`products\` WHERE \`id\`=${req.params.product}`, function (err, result) {
+        if (err) throw err;
+        res.status(202).json(result).end()
+    })
 }
 
 module.exports = {
